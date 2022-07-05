7e6cf53f325192482603a823532d6f469b3728b3
dhslrl321 <dhslrl321@gmail.com>
2022-07-06 08:32:42 +0900

    commit

---
+++ ch01-jpa/README.md
@@ -0,0 +1,3 @@
+# DDD With JPA
+
+- JPA 를 이용해서 Aggregate 를 구성합니다
---
+++ ch01-jpa/src/test/java/com/example/ch01jpa/Ch01JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ch01jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch01JpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
