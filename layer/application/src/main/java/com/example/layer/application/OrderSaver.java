73be178c7af9fbd6f1ba727114dcd04d5691dc5c
dhslrl321 <dhslrl321@gmail.com>
2022-07-31 17:27:22 +0900

    commit

--- layer/application/src/main/java/com/example/layer/application/OrderSaver.java
+++ layer/application/src/main/java/com/example/layer/application/OrderSaver.java
@@ -3,6 +3,7 @@ package com.example.layer.application;
 import com.example.layer.core.Order;
 import com.example.layer.model.SavedUser;
 import com.example.layer.repository.OrderRepository;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,12 +11,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class OrderSaver {
 
-    private static final String[] addressCandidates = {"seoul", "busan", "yeosu", "tokyo", "los angeles", "new york"};
-    private int count = 0;
     private final OrderRepository repository;
 
     public SavedUser save() {
-        Order order = Order.builder().address(addressCandidates[count++]).build();
+        Order order = Order.builder().address(UUID.randomUUID().toString()).build();
         Order saved = repository.save(order);
         return SavedUser.of(saved.getId(), saved.getAddress());
     }
